2524bbbe0c14c4c0bbd1f0be6d73f2380ae64acc
Muhammad-Yuda-Yudistira <yudistira22112022@gmail.com>
2023-01-03 23:03:45 +0700

    Belajar reactJS with buildwithangga

--- src/App.js
+++ src/App.js
@@ -3,23 +3,61 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Parentbox'>
+      <FotoProduk />
+      <ProdukInfo isDiscount="yes" name="Nike Sport" category="LEBARAN"/>
     </div>
   );
+}
+function FotoProduk() {
+  return (
+    <div className='Foto'>
+      <img src='shoes.jpg'></img>
+    </div>
+  );
+}
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if(isDiscount == "yes") {
+    return (
+      <p>Diskon 50% off</p>
+    );
+  } 
+  else if(isDiscount == "coming") {
+    return (
+      <p>Akan ada diskon ...</p>
+    );
+  }
+  else {
+    return (
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+function ProdukInfo(props) {
+  const {category, name, isDiscount} = props;
+  const benefits = ["Tidak kusut terkena air", "Bahan lebih halus", "Tidak gerah"]
+  const listBenefit = benefits.map((itemBenefit) => 
+    <li>{itemBenefit}</li>
+  );
+  return (
+    <div className='Deskripsi'>
+      <p className='Cate'>{category}</p>
+      <h1 className='Title'>{name}</h1>
+      <p className='Price'>IDR 1.200.000</p>
+      <CheckDiscount isDiscount={isDiscount} />
+      <p className='Info'>
+        Sneaker berwarna merah, untuk olahraga, ada berbagai ukuran bisa untuk pria dan wanita.
+      </p>
+      <ul>
+        {listBenefit}
+      </ul>
+      <a onClick={(e) => TambahCart(name, e)} href="#">Add to cart</a>
+    </div>
+  );
+}
+function TambahCart(e) {
+  console.log("Membeli " + e)
 }
 
 export default App;
